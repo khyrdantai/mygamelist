5a4f59a37c3298d776b64f2a32786638465acd9d
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-07-07 16:23:04 -0400

    added controller folder and moved login api

--- server.js
+++ server.js
@@ -3,45 +3,6 @@
 const {connectDB, express, path, PORT, app, client} = require("./backend/config/db");
 connectDB();
 
-app.post('/api/login', async (req, res, next) => 
-{
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-
-  let error = '';
-
-  const { login, password } = req.body;
-
-  const db = client.db("MyGameListDB");
-  const results = await db.collection('Users').find({login:login,password:password}).toArray();
-
-  let id = -1;
-  let fn = '';
-  let ln = '';
-
-  if( results.length > 0 )
-  {
-    id = results[0]._id;
-    fn = results[0].firstName;
-    ln = results[0].lastName;
-  }
-
-  let ret = { id:id, firstName:fn, lastName:ln, error: error};
-  res.status(200).json(ret);
-});
-
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') 
-{
-  // Set static folder
-  app.use(express.static('frontend/build'));
-
-  app.get('*', (req, res) => 
- {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
 //start Node + Express server listener
 app.listen(PORT, () => 
 {
