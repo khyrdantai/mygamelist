225a1c8d22b37a2eef5cf83d4ea07e62f650f3ad
shafferj2019 <shafferj2019@knights.ucf.edu>
2022-07-09 17:12:34 -0400

    Move production server statics assets assignment from steamRoute.js to server.js

--- server.js
+++ server.js
@@ -15,3 +15,15 @@ app.listen(PORT, () =>
 {
   console.log('Server listening on port ' + PORT);
 });
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
