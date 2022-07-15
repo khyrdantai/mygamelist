8e25bb6a2b9ea14b3eade9154747b8d53f96b0ef
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-07-15 12:56:19 -0400

    refactored authentication code

--- backend/authentication.js
+++ backend/authentication.js
@@ -1,10 +1,27 @@
 const jwt = require('jsonwebtoken')
+const {user, initial_key} = require("./routes/loginRoute")
 
+// generic function for generating 64 bit keystrings with hex
 const generate_key = function() {
     return require('crypto').randomBytes(64).toString('hex')
 }
 
+// middleware function for authenticating a user
+const authenticate_token = function(req, res, next){
+    const auth_header = req.headers['authorization']
+    const token = auth_header && auth_header.split(' ')[1]
+    
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, initial_key,(err, user)=>{
+      if(err) return res.sendStatus(403)
+      req.user = user
+      next()
+    })
+
+  }
+
 module.exports = 
 {
-    jwt, generate_key 
+    jwt, generate_key, authenticate_token 
 }
--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -1,69 +1,32 @@
 // local modules
 const {client, express} = require("../db");
 const login_router = express.Router();
-const {jwt, generate_key} = require("../authentication")
+const {jwt, generate_key, get_token} = require("../authentication")
+
+const user = null;
+let initial_key = generate_key();
 
 //login api
 login_router.post('/', async (req, res) => {
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
 
-    let error = '';
+  const { userName, password } = req.body;
+  user = {userName:userName,password:password}
 
-    const { userName, password } = req.body;
-    const user = {userName:userName,password:password}
-    let initial_key = generate_key()
+  // create jwt token
+  let token = get_token()     
 
-    let get_token = function(){
-      console.log(initial_key + "  <- this is the value saved in initial key")
-      return jwt.sign(user, initial_key)
-    }
+  //connect to and access db
+  const db = client.db("MyGameListDB");
+  const results = await db.collection('Users').find(user).toArray();
 
-    let full_token = get_token()
-    
-    // this is middleware that needs to be used by other api calls but i dont have it quite figured out yet
-    function authenticate_token(req, res, next){
-      const auth_header = req.headers['authorization']
-      const token = auth_header && auth_header.split(' ')[1]
-      
-      // no token no access
-      if(token == null) return res.sendStatus(401)
-
-      jwt.verify(token, initial_key,(err, user)=>{
-        if(err) return res.sendStatus(403)
-        req.user = user
-        next()
-      })
-
-    } 
-
-    const db = client.db("MyGameListDB");
-
-    const results = await db.collection('Users').find(user).toArray();
-
-
-    let id = -1;
-    let fn = '';
-    let ln = '';
-    let un = '';
-    let local_email = '';
-
-    if( results.length > 0 )
-    {
-      id = results[0]._id;
-      fn = results[0].firstName;
-      ln = results[0].lastName;
-      un = results[0].userName;
-      local_email = results[0].email;
-    }
-
-
-    let ret = {accessToken: full_token};
-    res.status(200).json(ret);
-  });
+  // the return of the login function is the jwt token. this needs to be stored locally somwhere
+  let ret = {accessToken: token};
+  res.status(200).json(ret);
+});
 
 module.exports = 
 {
   login_router, 
-  authenticate_token
+  user,
+  initial_key
 }
