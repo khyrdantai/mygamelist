ef317ffaf0c419875a0b1fd3c26f7c21fa5f88c1
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-07-15 15:03:58 -0400

    this shit does not work

--- backend/routes/addGameRoute.js
+++ backend/routes/addGameRoute.js
@@ -3,17 +3,18 @@ const add_game_router = express.Router()
 const authenticate_token = require('../authentication')
 
 add_game_router.post('/',  async (req, res) =>{
-    error=''
     const this_game_id = req.body.this_game_id
     const this_game_rating = req.body.this_game_rating
     const user_id = req.body.user_id;
 
+    const filter = {_id:user_id}
+    const update = { $push: {"games": {id:this_game_id, rating: this_game_rating},}}
+    const options = {upsert: true}
+
     const db = client.db("MyGameListDB");
-    const result = await db.collection('Users').updateOne({_id: user_id} , { $push: {"games": {id:this_game_id, rating: this_game_rating}}})
+    const result = await db.collection('Users').updateOne(filter , update, options)
     
-    console.log(result);
-    let ret = {"hello":"world"}
-    
+    console.log(result);    
     res.status(200).json(result);
 })
 
