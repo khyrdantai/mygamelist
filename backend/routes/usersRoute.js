2f2db43e222ec4deef19535c71f97cff58f4cb90
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2022-07-17 19:25:38 -0400

    cryptography I N I T I A T E D

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,7 +1,9 @@
 // modules
 const mongoose = require('mongoose');
 const {client, express} = require("../db");
-const {get_token, jwt, initial_key} = require("../authentication")
+const {jwt, initial_key} = require("../authentication")
+const bcrypt = require('bcrypt');
+const { json } = require('body-parser');
 
 //router to export
 const users_router = express.Router();
@@ -9,59 +11,79 @@ const users_router = express.Router();
 //register api
 users_router.post('/register', async (req, res) =>{
 
-  // new user data    
-  let  _id = new mongoose.Types.ObjectId()
-  let  firstName = req.body.firstName
-  let  lastName = req.body.lastName
-  let  userName = req.body.userName
-  let  password = req.body.password
-  let  email = req.body.email
-  
-  const db = client.db("MyGameListDB");
+  try{
+    //hash password
+    const salt = await bcrypt.genSalt()
+    const hashed_password = await(req.body.password, salt)
+    
+    // new user data 
+    let  _id = new mongoose.Types.ObjectId()
+    let  firstName = req.body.firstName
+    let  lastName = req.body.lastName
+    let  userName = req.body.userName
+    let  email = req.body.email
+    
+    const db = client.db("MyGameListDB");
 
-  const usernameCheck = await db.collection('Users').find({userName: userName}).toArray();
-  const emailCheck = await db.collection('Users').find({email: email}).toArray();
+    const usernameCheck = await db.collection('Users').find({userName: userName}).toArray();
+    const emailCheck = await db.collection('Users').find({email: email}).toArray();
 
-  if (usernameCheck.length > 0 && emailCheck.length > 0)
-  {
-    res.status(409).send('Username and email already exists');
-  }
-  else if (usernameCheck.length > 0)
-  {
-    res.status(409).send('Username already exists');
-  }
-  else if (emailCheck.length > 0)
-  {
-    res.status(409).send('Email already exists');
-  }
-  else
-  {
+    if (usernameCheck.length > 0 && emailCheck.length > 0)
+    {
+      res.status(409).send('Username and email already exists');
+    }
+    else if (usernameCheck.length > 0)
+    {
+      res.status(409).send('Username already exists');
+    }
+    else if (emailCheck.length > 0)
+    {
+      res.status(409).send('Email already exists');
+    }
+    else
+    {
+      // insert new user into database
+      const add_user = await db.collection('Users').insertOne({_id:_id,firstName:firstName, lastName:lastName,password:hashed_password,email:email,userName:userName})
+      
+      res.status(200).json({message: "added new user"});
 
-  // insert new user into database
-  const add_user = await db.collection('Users').insertOne({_id:_id,firstName:firstName, lastName:lastName,password:password,email:email,userName:userName})
-  
-  res.status(200).json({
-    message: "Registered new user"
-  });
+    }
+  }catch{
+  res.sendStatus(500).send
   }
 })
 
 //login api
 users_router.post('/login', async (req, res) => {
 
-  const {userName, password} = req.body
-  const user = {userName:userName, password:password} 
-
   //connect to and access db
   const db = client.db("MyGameListDB");
-  const RETURN_USER = await db.collection('Users').find(user).project({password: 0 , games: 0}).toArray();
+  
+  const user = {userName: req.body.userName}
+  const RETURN_USER = await db.collection('Users').find(user).project({firstName: 0, lastName: 0 , email:0, games: 0}).toArray()
 
-  jwt.sign({user:RETURN_USER}, initial_key, (err, token) =>{
-      res.json({
-        token:token
-      });
+  if(RETURN_USER == null){
+    return res.status(400).send("cannot find user")
+  }
+
+  const password = String(req.body.password)
+  const hash = String(RETURN_USER[0].password)
+
+  try{
+    if(async ()=>{bcrypt.compare(password, hash)} ){
+      jwt.sign({user:RETURN_USER}, initial_key, (err, token) =>{
+        res.json({
+          token:token
+        });
+      })
+    }
     
-  })
+  }catch{
+    res.status(500).send()
+  }
+
+
+  
 });
 
 module.exports = users_router
