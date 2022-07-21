cb635ce738f69007afbcc8c12f782757f649c560
shafferj2019 <shafferj2019@knights.ucf.edu>
2022-07-21 14:30:18 -0400

    Add try/catch around APIs

--- backend/routes/emailRoute.js
+++ backend/routes/emailRoute.js
@@ -8,29 +8,36 @@ sgMail.setApiKey(SENDGRID_API_KEY);
 //Call SendGrid's API to send the the verification email
 email_router.post('/sendVerification', (req, res) =>
 {
-    const msg = {
-        from: 'shafferj2019@knights.ucf.edu',
-        templateId: 'd-ca92eef5f9e643a684dd04bf78448148',
-        personalizations: [{
-            dynamic_template_data: {
-                userId: req.body.id,
-                firstName: req.body.firstName
-            },
-            to: req.body.email
-        }]
-    }
-
-    sgMail.send(msg, (error, result) => 
+    try
     {
-        if(error) 
-        {
-            res.status(404).send('Email not found');
+        const msg = {
+            from: 'shafferj2019@knights.ucf.edu',
+            templateId: 'd-ca92eef5f9e643a684dd04bf78448148',
+            personalizations: [{
+                dynamic_template_data: {
+                    userId: req.body.id,
+                    firstName: req.body.firstName
+                },
+                to: req.body.email
+            }]
         }
-        else
+    
+        sgMail.send(msg, (error, result) => 
         {
-            res.status(200).send('Email sent');
-        }
-    });
+            if(error) 
+            {
+                res.status(404).send('Email not found');
+            }
+            else
+            {
+                res.status(200).send('Email sent');
+            }
+        });
+    }
+    catch(e)
+    {
+      res.status(404).send(e);
+    }
 })
 
 //Call SendGrid's API to send the password reset email
